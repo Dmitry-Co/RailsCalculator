d50c89195c00119318054eaf60f30504da6b5a4c
Dmitry-Co <sechko.dp@gmail.com>
2023-11-16 09:48:25 +0500

    feat: add Index method Controller

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,20 @@
 class CalculatorController < ApplicationController
   def index
+    num1 = params[:num1].to_f
+    eq = params[:eq]
+    num2 = params[:num2].to_f
+
+    @result = case eq
+              when '*'
+                num1 * num2
+              when '/'
+                num1 / num2
+              when '+'
+                num1 + num2
+              when '-'
+                num1 - num2
+              else
+                "Sorry, wrong operator!"
+              end
   end
 end
